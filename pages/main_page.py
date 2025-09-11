a74acebf56906ed9c8dafbbc2d5b3639c62736c4
Пользователь <maryfedoseeva083@gmail.com>
2025-09-11 06:18:18 +0300

    Add LoginPage with locators and update BasePage/MainPage tests

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
@@ -8,4 +9,4 @@ class MainPage(BasePage):
         login_link.click()
 
     def should_be_login_link(self):
-        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
